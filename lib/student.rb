3eca99197d6032319c0fbff006d42aa5ec307c9d
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-29 08:07:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,6 +35,13 @@ class Student
     end
   end
   
-  
+  def self.new_from_db(row)
+    # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
 
 end
