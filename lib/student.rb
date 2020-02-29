633c65ce98679fa9ab576fd3cf8abdbec0ee95c0
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-29 08:25:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,10 +5,9 @@ class Student
   attr_accessor :name, :grade, :id 
   
   def initialize(name, grade, id=nil)
-    
+    @id = id
     @name = name
     @grade = grade
-    
   end
   
   def self.create_table
